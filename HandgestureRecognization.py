4fd5e904140224a922eea0fcb9b05781ae5c5984
SaiVinay007 <g.saivinay007@gmail.com>
2018-09-08 20:01:38 +0530

    intial commit

--- HandgestureRecognization.py
+++ HandgestureRecognization.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-Hand_image = cv2.imread('/home/saivinay/Documents/hand_gesture/five-hand-image.jpg')
+Hand_image = cv2.imread('/home/saivinay/Documents/hand_gesture/four.jpeg')
 image_YCrCb_format = cv2.cvtColor(Hand_image,cv2.COLOR_BGR2YCrCb)
 
 #  Taking the general range of skin color 
@@ -17,7 +17,13 @@ edges = cv2.Canny(mask,100,200)
 # To make the un conneted points connected to the nearest points
 gaussian = cv2.GaussianBlur(mask,(5,5),0)
 
+
 # Returns all the contours present
+# Each individual contour is a Numpy array of (x,y) coordinates of boundary points of the object.
+# If you pass cv2.CHAIN_APPROX_NONE, all the boundary points are stored. But actually do we need all the points? 
+# For eg, you found the contour of a straight line. Do you need all the points on the line to represent that line? 
+# No, we need just two end points of that line. This is what cv2.CHAIN_APPROX_SIMPLE does. It removes all redundant points and compresses the contour, thereby saving memory           
+
 image, contours, hierarchy = cv2.findContours(gaussian,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
 
@@ -32,18 +38,44 @@ for i,contour in enumerate(contours):
 
 
 max_area = cv2.contourArea(contours[num])
-print(max_area)
+# print(max_area)
 
 
-draw_contours = cv2.drawContours(roi_image, contours, num, (0,255,0), 3)
-print(len(draw_contours))
+# To draw all contours pass -1 in place of num
+draw_contours = roi_image
+cv2.drawContours(draw_contours, contours, num, (255,0,0), 3)
+# print(len(draw_contours))
+
+# Used for filling the space inside of the contour
+cv2.fillPoly(draw_contours, pts =[contours[num]], color=(255,255,255))
+
+check_convexity = gaussian
+hull = cv2.convexHull(contours[num],returnPoints = False)
+defects = cv2.convexityDefects(contours[num],hull)
+print((defects))
+# print(hull)
+print(defects.shape[0])
+
+for i in range(defects.shape[0]):
+    s,e,f,d = defects[i,0]
+    start = tuple(contours[num][s][0])
+    end = tuple(contours[num][e][0])
+    far = tuple(contours[num][f][0])
+    cv2.line(draw_contours,start,end,[0,255,0],2)
+    cv2.circle(draw_contours,far,5,[0,0,255],-1)
+
+
 
 cv2.imshow('gaussian',gaussian)
 # cv2.imshow('image_YCrCb_format',image_YCrCb_format)
 # cv2.imshow('mask',mask)
-cv2.imshow('roi_image',roi_image)
-# cv2.imshow('draw_contours',draw_contours)
+# cv2.imshow('roi_image',roi_image)
+cv2.imshow('draw_contours',draw_contours)
 # cv2.imshow('edges',edges)
+
+
+
+cv2.imshow('defects',defects)
 
 k = cv2.waitKey(70000) & 0xFF
 cv2.destroyAllWindows()
